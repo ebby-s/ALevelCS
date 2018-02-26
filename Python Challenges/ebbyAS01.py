29c3f45944f2959c03c48a23430d88d3dda9138b
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-02-26 10:57:19 +0800

    Update ebbyAS01.py

--- Python Challenges/ebbyAS01.py
+++ Python Challenges/ebbyAS01.py
@@ -1,10 +1,11 @@
 num = 0
 nlist = [1,6,9,3,5,11]
 
-for i in range(0,len(nlist)-1,1):
-    if nlist[i] > nlist[i+1]:
-        num = nlist[i]
-        nlist[i]=nlist[i+1]
-        nlist[i+1] = num
+for j in range(len(nlist)):
+    for i in range(j):
+        if nlist[i] > nlist[i+1]:
+            num = nlist[i]
+            nlist[i]=nlist[i+1]
+            nlist[i+1] = num
 
 print(nlist)
