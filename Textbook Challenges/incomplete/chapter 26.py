7d2724f3aa320f5cd7d0fd97bba6b07ec6c25080
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-10-30 13:06:42 +0800

    Add files via upload

--- Textbook Challenges/incomplete/chapter 26.py
+++ Textbook Challenges/incomplete/chapter 26.py
@@ -15,3 +15,34 @@ class list_queue:
         return self.items == []
 
 
+class Node:
+    def __init__(self, cargo=None, next_node=None):
+        self.cargo = cargo
+        self.next  = next_node
+
+    def __str__(self):
+
+class linked_queue:
+
+    def __init__(self):
+        self.items = None
+
+    def insert(self,item,node=None,first=True):
+        if first:
+            node = self.items
+            if node <= item:
+                item.next = node
+                self.items = item
+                return
+            self.insert(item,node)
+        if node <= item:
+                item.next = node
+                self.items = item
+                return
+            self.insert(item,node)
+        
+
+    def remove(self):
+        item = str(self.items)
+        self.items = self.items.next
+        return str(item)
