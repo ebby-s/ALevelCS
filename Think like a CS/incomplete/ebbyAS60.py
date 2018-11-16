d72c59284cf231d162eec918ef43a17b6a6de8be
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-11-16 15:22:09 +0800

    Update Think like a CS/incomplete/ebbyAS60.py

--- Think like a CS/incomplete/ebbyAS60.py
+++ Think like a CS/incomplete/ebbyAS60.py
@@ -4,6 +4,15 @@
 
 def checker(n,m):
     board = [[x for x in range(m)] for y in range(n)]
+    for i in range(len(board)):
+        if i%2 == 0:
+            start = 0
+        else:
+            start = 1
+        for j in range(len(board[i])):
+            if (start+board[i][j])%2 == 0:
+                board[i][j] = "."
+            else:
+                board[i][j] = "*"
     for row in board:
-        for element in row:
-            
+        print(row)
