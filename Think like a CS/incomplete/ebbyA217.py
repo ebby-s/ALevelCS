f6818c4ff74e5e52d62935adb2864506c2f630b6
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-11-16 16:29:52 +0800

    Create ebbyA217.py

---
+++ Think like a CS/incomplete/ebbyA217.py
@@ -0,0 +1,61 @@
+import random, sys, os, math
+
+def main():
+    makeKeyFiles('al_sweigart', 1024)
+
+def isPrime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generateLargePrime(keysize=1024):
+    while True:
+        num = random.randrange(2**(keysize-1), 2**(keysize))
+        if isPrime(num):
+            return num
+
+def gcd(a, b):
+    while a != 0:
+        a, b = b % a, a
+    return b
+
+def findModInverse(a, m):
+    if gcd(a, m) != 1:
+        return None
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+
+def generateKey(keySize):
+    p = generateLargePrime(keySize)
+    q = generateLargePrime(keySize)
+    n = p * q
+
+    while True:
+        e = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
+        if gcd(e, (p - 1) * (q - 1)) == 1:
+            break
+
+    d = findModInverse(e, (p - 1) * (q - 1))
+    publicKey = (n, e)
+    privateKey = (n, d)
+    print('Public key:',publicKey)
+    print('Private key:',privateKey)
+    return (publicKey, privateKey)
+
+def makeKeyFiles(name, keySize):
+    if os.path.exists('%s_pubkey.txt' % (name)) or os.path.exists('%s_privkey.txt' % (name)):
+        sys.exit('WARNING: The file %s_pubkey.txt or %s_privkey.txt already exists! Use a different name or delete these files and re-run this program.' % (name, name))
+    publicKey, privateKey = generateKey(keySize)
+    fo = open('%s_pubkey.txt' % (name), 'w')
+    fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
+    fo.close()
+    fo = open('%s_privkey.txt' % (name), 'w')
+    fo.write('%s,%s,%s' % (keySize, privateKey[0], privateKey[1]))
+    fo.close()
