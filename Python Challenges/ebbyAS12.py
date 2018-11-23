8ef15fb4cf8ac3226500e09b7b4befb6d79844d2
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-11-23 17:48:08 +0800

    Create ebbyAS12.py

---
+++ Python Challenges/ebbyAS12.py
@@ -0,0 +1,19 @@
+conv_pseudocode = {"int":"INTEGER","float":"REAL","char":"CHAR","str":"STRING","bool":"BOOLEAN","list":"ARRAY",
+                   "open":"OPENFILE","read()":"READFILE","write":"WRITEFILE",
+                   "print":"OUTPUT","if":"IF","else":"ELSE","elif":"CASE OF",
+                   "for":"FOR","while":"WHILE","def":"FUNCTION","type":"TYPE",
+                   "=":"<--","==":"=","in":"IN","range":"RANGE"}
+
+code = open("python code.txt")
+lines = code.readlines()
+
+output = open("pseudocode.txt","w")
+for line in lines:
+    p_line = ''
+    for part in line.split(" "):
+        if part in conv_pseudocode.keys():
+            part = conv_pseudocode[part]
+        p_line += part+" "
+    output.write(p_line)
+output.close()
+
