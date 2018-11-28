a90c3b0b80a7cdd8f9d62a2028389431f7516b8b
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-11-28 13:22:29 +0800

    Update Task 3.py

--- 2019 mock prerelease/Task 3.py
+++ 2019 mock prerelease/Task 3.py
@@ -1,7 +1,7 @@
 import pickle
 
 def LeftToInt(phrase,n):
-    return int(phrase[0,n])
+    return int(phrase[0:n])
 
 def Hash(isbn):
     address = LeftToInt(isbn,3)
@@ -13,7 +13,7 @@ class BookRecord:
         self.title = None
         self.loan = None
     def __str__(self):
-        return "{0} {1} {2}".format(self.isbm,self.title,self.loan)
+        return "{0} {1} {2}".format(self.isbn,self.title,self.loan)
 
 dummyRecord = BookRecord()
 dummyRecord.isbn = "0000000000"
@@ -21,4 +21,23 @@ dummyRecord.isbn = "0000000000"
 bookFile = open("BookFile.txt","wb")
 for i in range(1,1001):
     pickle.dump(dummyRecord,bookFile)
+bookFile.close()
+
+
+newBook = BookRecord()
+
+bookFile = open("Bookfile.txt","ab")
+for i in range(5):
+    newBook.isbn = input("Enter ISBN: ")
+    newBook.title = input("Enter title: ")
+    address = Hash(newBook.isbn)
+    bookFile.seek(address)
+    pickle.dump(newBook,bookFile)
+bookFile.close()
+
+
+bookFile = open("BookFile.txt","rb")
+for i in range(1000):
+    record = pickle.load(bookFile)
+    if record.isbn != "0000000000": print(str(record))
 bookFile.close()
