c5f757f96ed65d4ef4e7df49ec22deb5d02f4220
ebby-s <31300178+ebby-s@users.noreply.github.com>
2019-01-26 12:54:27 +0800

    Create P2 Task 1.2.py

---
+++ 2019 prerelease/P2 Task 1.2.py
@@ -0,0 +1,23 @@
+def yes(text):
+    if input(text) in ["Y","y","yes"]: return True
+    else: return False
+
+def input_record():
+    name = str(input("Enter name: "))
+    email = str(input("Enter email address: "))
+    return name+"#"+email
+
+def print_list(array):
+    array.insert(0,"Name#Email")
+    for record in array:
+        if record == "#": continue
+        [name,email] = record.split("#")
+        print("{0:>20} {1:>20}".format(name,email))
+
+StudentList = []
+
+while yes("Would you like to input another record?"):
+    record = input_record()
+    StudentList.append(record)
+
+print_list(StudentList)
