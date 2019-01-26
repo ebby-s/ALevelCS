1f638708f334b277f6e12de9aa35ab9595626f17
ebby-s <31300178+ebby-s@users.noreply.github.com>
2019-01-26 12:54:25 +0800

    Update P2 Task 1.1.py

--- 2019 prerelease/P2 Task 1.1.py
+++ 2019 prerelease/P2 Task 1.1.py
@@ -1,27 +1,13 @@
-def is_int(char):
-    try:
-        char = int(char)
-        return True
-    except: return False
-
 def yes(text):
     if input(text) in ["Y","y","yes"]: return True
     else: return False
 
 def input_record():
-    name = str(input("Enter Student ID: "))
+    name = str(input("Enter name: "))
     email = str(input("Enter email address: "))
     return name+"#"+email
 
-def valid(text):
-    [name,email] = text.split("#")
-    check = []
-    for char in name: check.append(is_int(char))
-    if check == [False,False,True,True,True,True]:
-        return True
-    else: return False
-
-def prnt_list(array):
+def print_list(array):
     array.insert(0,"Name#Email")
     for record in array:
         [name,email] = record.split("#")
@@ -31,6 +17,7 @@ StudentList = []
 
 while yes("Would you like to input another record?"):
     record = input_record()
-    if valid(record): StudentList.append(record)
-    else: print("Invalid information")
+    StudentList.append(record)
+
+print_list(StudentList)
 
