fef0fbf5519322eabca0610069221e3dbce84f4c
ebby-s <31300178+ebby-s@users.noreply.github.com>
2019-01-27 11:34:17 +0800

    Create P2 Task 2.4a.py

---
+++ 2019 prerelease/P2 Task 2.4a.py
@@ -0,0 +1,32 @@
+def is_int(char):
+    try:
+        char = int(char)
+        return True
+    except: return False
+
+def yes(text):
+    if input(text) in ["Y","y","yes"]: return True
+    else: return False
+
+def input_record():
+    student_id = str(input("Enter Student ID: "))
+    email = str(input("Enter email address: "))
+    dob = str(input("Enter date of birth: "))
+    return student_id+"#"+email+"#"+dob
+
+def valid(text):
+    [student_id,email,dob] = text.split("#")
+    check = []
+    for char in student_id: check.append(is_int(char))
+    if check == [False,False,True,True,True,True]:
+        return True
+    else: return False
+    
+StudentFile = open("Student File.txt","w")
+
+while yes("Would you like to input another record?"):
+    record = input_record()
+    if valid(record): StudentFile.write(record)
+    else: print("Invalid information")
+
+StudentFile.close()
