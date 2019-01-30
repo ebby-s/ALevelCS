f8a2fcb829fa0c975a8403ec362e6611ce33859c
Ebby Samson <ebbysamson@gmail.com>
2019-01-30 11:49:43 +0800

    Update igcse prerelease.py

--- 2019 prerelease/igcse prerelease.py
+++ 2019 prerelease/igcse prerelease.py
@@ -27,10 +27,6 @@ class item:
         if len(self.bids) == 0: return 0
         else: return self.bids[-1][1]
 
-def print_table(layout,text):
-    for row in text:
-        print()
-
 def find_sold(items):
     sold = []
     unsold = []
@@ -39,9 +35,38 @@ def find_sold(items):
         else: unsold.append(item)
     return [sold,unsold]
 
+def main_menu():
+    print("__________________")
+    print("Menu")
+    print()
+    print("1. Show items")
+    print("2. Bid on item")
+    print("3. Quit")
+    try: return int(input(" : "))
+    except:
+        print("Invalid choice")
+        return main_menu()
+
+def show_items(items):
+    layout = "{0:>3} {1:>15} {2:>25} {3:>12}"
+    print(layout.format(" ","Item number","Description","Highest bid"))
+    for i,item in enumerate(items):
+        print(layout.format(i,int(item),str(item),item.highest_bid()))
+
+def take_bids(items):
+    try: item = int(input("Choose an item: "))
+
+def step1(items):
+    while True:
+        choice = main_menu()
+        if choice == 1: show_items(items)
+        elif choice == 2:
+            
+            
+
 items = []
 
-layout = "{0:>2} {1:>13} {2:>25} {3:>5}"
+
 
 
 
